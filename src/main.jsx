4d2cb5cdc2d8277bc18827357de3805f70139da8
AKumar1998 <ajaybrowning13@gmail.com>
2025-08-10 14:37:46 +0530

    tailwind installed - tested npm run dev

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const Test = () => {
+  return <div>
+    Hello World
+  </div>
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Test/>
   </StrictMode>,
 )
